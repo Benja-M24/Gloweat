7052bb445530bff1cafee3b46c1be0ad389d425c
Lucho Gomez <lucho.gomez2006@hotmail.com>
2019-11-07 20:44:52 -0300

    Nuevos Recursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
+//Componentes
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { InicioComponent } from './inicio/inicio.component';
+
+
+//Modulos Material
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './inicio/inicio.component';
 import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {LayoutModule} from '@angular/cdk/layout';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    SidenavComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    LayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
