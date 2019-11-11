27277843c932878c05b0f8f4c74f11d5a5fb9163
benja-MM <benjamarmol24@gmail.com>
2019-11-11 17:10:50 -0300

    se suma el front trabajado por lucho

--- FrontGl/src/app/app.module.ts
+++ FrontGl/src/app/app.module.ts
@@ -1,16 +1,45 @@
+//Componentes
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { InicioComponent } from './inicio/inicio.component';
+
+
+//Modulos Material
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {LayoutModule} from '@angular/cdk/layout';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { LoginComponent } from './login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SidenavComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
