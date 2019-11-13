09501afe16842bb6d2b9a2eda5bd9d09a68acc91
Lucho Gomez <lucho.gomez2006@hotmail.com>
2019-11-13 20:05:04 -0300

    componentes terminados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,43 @@
 //Componentes
 
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { InicioComponent } from './inicio/inicio.component';
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { LoginComponent } from './login/login.component';
 
 
 //Modulos Material
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {LayoutModule} from '@angular/cdk/layout';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
+import {RouterModule, Routes} from '@angular/router';
+import { ProductosComponent } from './productos/productos.component';
+import { ElaboracionComponent } from './elaboracion/elaboracion.component';
+
+const routes: Routes = [
+  // {path: '', component: SidenavComponent},
+  // {path: '/login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     SidenavComponent,
+    LoginComponent,
+    ProductosComponent,
+    ElaboracionComponent,
 
   ],
   imports: [
@@ -32,7 +48,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatSidenavModule,
     MatListModule,
     LayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/elaboracion/elaboracion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elaboracion',
+  templateUrl: './elaboracion.component.html',
+  styleUrls: ['./elaboracion.component.css']
+})
+export class ElaboracionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
